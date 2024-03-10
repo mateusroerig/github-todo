dcf82ac3e476b73646c6402376e6b02c3ea4f20a
Mateus Roerig <mateusroerig@outlook.com>
2024-03-09 21:25:37 -0300

    feat: Adicionar estruturas do projeto

---
+++ github-todo-api/src/main/java/br/unisinos/githubtodoapi/GithubTodoApiApplication.java
@@ -0,0 +1,13 @@
+package br.unisinos.githubtodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTodoApiApplication.class, args);
+	}
+
+}
---
+++ github-todo-api/src/test/java/br/unisinos/githubtodoapi/GithubTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisinos.githubtodoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
