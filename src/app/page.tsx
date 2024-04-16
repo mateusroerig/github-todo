4814465a3daa795b4735be1c94173246fefbd77c
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-04-16 15:15:39 -0300

    refact: main page component to include layout and login functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "antd";
+import { Button, Layout } from "antd";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
@@ -8,19 +8,49 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <main className="flex justify-center items-center h-screen">
-      <div>
-        <Button
-          type="primary"
-          loading={loading}
-          onClick={() => {
-            setLoading(true);
-            signIn("github", { callbackUrl: "/dashboard" });
-          }}
-        >
-          Login
-        </Button>
-      </div>
-    </main>
+    <Layout className="h-screen">
+      <Layout.Header className="flex justify-between items-center">
+        <h1>GitHub Todo</h1>
+
+        <div className="flex gap-4 justify-center items-center">
+          <a href="/about">About</a>
+
+          <Button
+            type="primary"
+            loading={loading}
+            onClick={() => {
+              setLoading(true);
+              signIn("github", { callbackUrl: "/dashboard" });
+            }}
+          >
+            Login
+          </Button>
+        </div>
+      </Layout.Header>
+
+      <Layout.Content className="flex grow flex-col gap-16 justify-center items-center">
+        <div className="w-1/2 h-1/2 bg-gray-200"></div>
+
+        <div className="flex flex-col gap-4 justify-center items-center">
+          <Button
+            type="primary"
+            loading={loading}
+            onClick={() => {
+              setLoading(true);
+              signIn("github", { callbackUrl: "/dashboard" });
+            }}
+          >
+            Login com Github
+          </Button>
+
+          <p>
+            Não possui uma conta?{" "}
+            <a href="https://github.com/signup" target="_blank" rel="noopener noreferrer">
+              Cadastre-se no Github.
+            </a>
+          </p>
+        </div>
+      </Layout.Content>
+    </Layout>
   );
 }
